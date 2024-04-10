efa4956745d17eae3f18318f1ef1ee61dd70067a
dir-FY24miyajimakotaro <kotaro.miyajima.q090@dirbato.co.jp>
2024-04-10 10:32:17 +0900

    コミット6です2

---
+++ Test/src/calc/CalculatorServiceTest.java
@@ -0,0 +1,24 @@
+package calc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorServiceTest {
+
+    @Test
+    void testCalculatorService() {
+        fail("まだ実装されていません");
+    }
+
+    @Test
+    void testAddAndReport() {
+        fail("まだ実装されていません");
+    }
+
+    @Test
+    void testSubtractAndReport() {
+        fail("まだ実装されていません");
+    }
+
+}
---
+++ Test/src/calc/CalculatorTest.java
@@ -0,0 +1,15 @@
+package calc;
+
+import junit.framework.TestCase;
+
+public class CalculatorTest extends TestCase {
+
+    public void testAdd() {
+        fail("まだ実装されていません");
+    }
+
+    public void testSubtract() {
+        fail("まだ実装されていません");
+    }
+
+}
